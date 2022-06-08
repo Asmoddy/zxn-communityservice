d5b0b676918423094fcc2782181435cef100c02c
Skywalker <83936914+DevKaedou@users.noreply.github.com>
2022-06-08 09:34:18 +0800

    Updated locations and added change clothing

--- config.lua
+++ config.lua
@@ -1,25 +1,49 @@
 Config = {}
 
-Config.SQL = "mysql"
+Config.SQL = "mysql" -- "mysql" or "ghmattimysql"
 
 Config.ServiceExtensionOnEscape		= 8
 
-Config.ServiceLocation 				= {x =  -342.45, y = 6173.65, z = 31.47, h = 180.0}
+Config.ServiceLocation 				= {x =  -442.18, y = 6017.37, z = 31.69, h = 315.0}
 
-Config.ReleaseLocation				= {x = -359.2, y = 6155.62, z = 31.47, h = 180.0}
+Config.ReleaseLocation				= {x = 426.14, y = -978.44, z = 30.71, h = 90.0}
 
 Config.ServiceLocations = {
-	{ type = "cleaning", coords = vector3(-352.73, 6152.12, 31.49) },
-	{ type = "cleaning", coords = vector3(-356.37, 6157.51, 31.49) },
-	{ type = "cleaning", coords = vector3(-348.36, 6161.23, 31.49) },
-	{ type = "cleaning", coords = vector3(-335.06, 6156.56, 31.49) },
-	{ type = "cleaning", coords = vector3(-333.67, 6151.17, 31.6) },
-	{ type = "cleaning", coords = vector3(-343.25, 6174.3, 31.41) },
-	{ type = "cleaning", coords = vector3(-336.59, 6179.04, 31.48) },
-	{ type = "cleaning", coords = vector3(-331.32, 6180.52, 32.14) },
-	{ type = "gardening", coords = vector3(-338.25, 6175.3, 31.69) },
-	{ type = "gardening", coords = vector3(-343.13, 6171.71, 31.64) },
-	{ type = "gardening", coords = vector3(-342.25, 6166.18, 31.49) },
-	{ type = "gardening", coords = vector3(-330.92, 6183.5, 31.64) },
-	{ type = "gardening", coords = vector3(-326.92, 6178.23, 32.5) }
+	{ type = "cleaning", coords = vector3(-445.21, 6024.12, 31.49) },
+	{ type = "cleaning", coords = vector3(-429.74, 6027.09, 31.49) },
+	{ type = "cleaning", coords = vector3(-435.96, 6023.48, 31.49) },
+	{ type = "cleaning", coords = vector3(-434.62, 6013.04, 31.49) },
+	{ type = "cleaning", coords = vector3(-429.98, 6008.11, 31.49) },
+	{ type = "cleaning", coords = vector3(-424.08, 6001.75, 31.49) },
+	{ type = "cleaning", coords = vector3(-438.47, 6030.11, 31.34) },
+	{ type = "cleaning", coords = vector3(-445.86, 6034.24, 31.34) },
+	{ type = "cleaning", coords = vector3(-445.31, 6040.75, 31.34) },
+	{ type = "cleaning", coords = vector3(-453.23, 6033.84, 31.34) },
+	{ type = "gardening", coords = vector3(-428.85, 6020.84, 31.65) },
+	{ type = "gardening", coords = vector3(-431.57, 6016.27, 31.62) },
+	{ type = "gardening", coords = vector3(-426.80, 6016.04, 31.66) },
+	{ type = "gardening", coords = vector3(-427.70, 6011.71, 31.62) },
+	{ type = "gardening", coords = vector3(-424.48, 6009.52, 31.63) },
+	{ type = "gardening", coords = vector3(-423.50, 6006.38, 31.57) }
+}
+
+Config.Uniforms = {
+	['male'] = {
+		outfitData = {
+			['t-shirt'] = {item = 15, texture = 0},
+			['torso2']  = {item = 345, texture = 0},
+			['arms']    = {item = 63, texture = 0},
+			['pants']   = {item = 27, texture = 2},
+			['shoes']   = {item = 5, texture = 0},
+		}
+	},
+	['female'] = {
+	 	outfitData = {
+			['t-shirt'] = {item = 15, texture = 0},
+			['torso2']  = {item = 141, texture = 2},
+			['arms']    = {item = 83, texture = 0},
+			['pants']   = {item = 3, texture = 15},
+			['shoes']   = {item = 16, texture = 0},
+	 	}
+	},
 }
